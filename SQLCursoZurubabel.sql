1d98a21e78541f67b4821b1c2495592afa2047e5
Diegormartins84 <diegormartins84@gmail.com>
2024-10-04 02:08:18 -0300

    Update SQLCursoZurubabel.sql

--- SQLCursoZurubabel.sql
+++ SQLCursoZurubabel.sql
@@ -1,3 +1,5 @@
+use dbLojaJailson;
+
 /*
 Nova base de dados
 */
@@ -850,10 +852,10 @@ Comparadores
 <> diferente
 > maior
 >= maior ou igual
-< menor
 <= menor ou igual
 */
 
+< menor
 SELECT TOP (1000) [BusinessEntityID]
       ,[NationalIDNumber]
       ,[LoginID]
@@ -893,7 +895,193 @@ SELECT TOP (1000) [BusinessEntityID]
       ,[ModifiedDate]
   FROM [AdventureWorks2017].[HumanResources].[Employee]
   --WHERE JobTitle = 'Design Engineer'
- -- WHERE JobTitle LIKE '%Engineer'
-  WHERE JobTitle LIKE '%Engineer%'
+ -- WHERE JobTitle LIKE '%Engineer' --ter Enginner no fim
+  WHERE JobTitle LIKE '%Engineer%' --ter Enginner
 
 ----------------------------------------------------------------
+
+-- Declarando variáveis
+DECLARE @x INT;
+SET @x = 2
+--print @x
+SELECT @x
+
+----------------------------------------------------------------
+
+-- Outro tipo de tabela temporária (variável)
+-- Não pode criar variável dentro de uma view
+
+DECLARE @tabela TABLE(
+	cdProduto INT PRIMARY KEY,
+	nmProduto VARCHAR(50)
+)
+
+INSERT INTO @tabela VALUES (1, 'Churros')
+
+SELECT * FROM @tabela
+
+----------------------------------------------------------------
+
+-- Operadores lógicos
+SELECT TOP (1000) [BusinessEntityID]
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+	  ,DATEDIFF(YEAR, BirthDate, GETDATE()) AS Idade
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2017].[HumanResources].[Employee]
+--WHERE DATEDIFF(YEAR, BirthDate, GETDATE()) > 40
+WHERE NOT (DATEDIFF(YEAR, BirthDate, GETDATE())) > 40
+--AND Gender = 'M'
+-- DATEDIFF(YEAR, HireDate, GETDATE()) > 2
+OR Gender = 'F'
+
+--Operadores:
+-- AND
+-- OR
+-- NOT
+
+----------------------------------------------------------------
+
+--use AdventureWorks2017;
+-- Bases utilizadas:
+--[AdventureWorks2017].[HumanResources].[Employee]
+--[AdventureWorks2017].[Person].[Person]
+
+-- Criando função de valor escalar
+--'CREATE FUNCTION' deve ser a primeira instrução em um lote de consultas
+
+CREATE FUNCTION UDF_NomeCompletoIdade(@BusinessEntityID INT)
+RETURNS VARCHAR(250)
+AS
+BEGIN
+
+	RETURN (SELECT DISTINCT
+		P.FirstName + ' ' + P.MiddleName + '. ' + P.LastName + ', ' + CAST(DATEDIFF(YEAR, E.BirthDate, GETDATE()) AS VARCHAR(4)) + ' anos'
+	FROM Person.Person AS P INNER JOIN HumanResources.Employee AS E
+	ON P.BusinessEntityID = E.BusinessEntityID
+	WHERE P.BusinessEntityID = @BusinessEntityID
+	)
+END
+
+SELECT AdventureWorks2017.dbo.UDF_NomeCompletoIdade(40)
+
+SELECT TOP (1000) [BusinessEntityID]
+	,AdventureWorks2017.dbo.UDF_NomeCompletoIdade(BusinessEntityID) AS NomeCompleto
+      ,[NationalIDNumber]
+      ,[LoginID]
+      ,[OrganizationNode]
+      ,[OrganizationLevel]
+      ,[JobTitle]
+      ,[BirthDate]
+      ,[MaritalStatus]
+      ,[Gender]
+      ,[HireDate]
+      ,[SalariedFlag]
+      ,[VacationHours]
+      ,[SickLeaveHours]
+      ,[CurrentFlag]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2017].[HumanResources].[Employee]
+
+  --sp_helptext UDF_NomeCompletoIdade
+
+----------------------------------------------------------------
+
+/*
+	Caso você tenha uma pasta para salvar seus scripts
+	Autor: Zurubabel
+	Data: 2024-10-04 01:41h
+	Base: AdventureWorks2017
+	Pasta:
+
+	Função que retorna os dados completos do funcionário.
+
+--sp_helptext UDF_DadosCompletosFuncionarios
+*/
+
+
+CREATE FUNCTION UDF_DadosCompletosFuncionarios()
+RETURNS @Retorno TABLE(
+	NomeCompleto VARCHAR(70),
+	Idade INT,
+	Nascimento DATE,
+	Sexo VARCHAR(1),
+	AnosCasa INT,
+	Cargo VARCHAR(50)
+)
+AS
+BEGIN
+
+INSERT INTO @Retorno
+
+	SELECT 
+		P.FirstName + ' ' + isnull(P.MiddleName, '') + '. ' + P.LastName AS [NomeCompleto],
+		DATEDIFF(YEAR, E.BirthDate, GETDATE()) AS Idade,
+		E.BirthDate AS Nascimento,
+		E.Gender AS Sexo,
+		DATEDIFF(YEAR, E.HireDate, GETDATE()) AS AnosCasa,
+		E.JobTitle AS Cargo
+	FROM Person.Person AS P INNER JOIN HumanResources.Employee AS E
+	ON P.BusinessEntityID = E.BusinessEntityID
+
+	RETURN
+END
+
+
+--sp_helptext UDF_DadosCompletosFuncionarios
+--SELECT * FROM AdventureWorks2017.dbo.UDF_DadosCompletosFuncionarios()
+----------------------------------------------------------------
+
+--Variáveis do cursor
+DECLARE @FirstName VARCHAR(50),
+		@MiddleName VARCHAR(50),
+		@LastName VARCHAR(50);
+		
+-- Criando cursores
+DECLARE cur_NomeCompleto CURSOR
+FOR SELECT top 10 FirstName, MiddleName, LastName FROM Person.Person
+--ORDER BY FirstName ASC
+
+-- Abrindo o curso
+OPEN cur_NomeCompleto
+
+-- Selecionar os dados
+FETCH NEXT FROM cur_NomeCompleto
+INTO @FirstName, @MiddleName, @LastName;
+
+--Iteração pelo dados retornados pelo cursor
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+	SELECT @FirstName + ' ' + ISNULL(@MiddleName, '') + ' ' + @LastName
+	
+	--Pegar os próximos dados
+	FETCH NEXT FROM cur_NomeCompleto
+	INTO @FirstName, @MiddleName, @LastName;
+
+END
+
+--
+
+SELECT @FirstName, @MiddleName, @LastName;
+
+-- Fechando e desalocando o cursor da memória
+CLOSE cur_NomeCompleto
+DEALLOCATE cur_NomeCompleto
+
+----------------------------------------------------------------
+
+
